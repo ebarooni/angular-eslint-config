7897722df75bd6c181e2ef742b0954df48fb09b5
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-12-30 21:13:50 +0100

    feat: add eslint config in package

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+export default {
+    external: [
+        "@eslint/js",
+        "angular-eslint",
+        "eslint",
+        "eslint-config-prettier",
+        "typescript",
+        "typescript-eslint",
+    ],
+    input: "src/index.js",
+    output: {
+        file: "dist/index.cjs",
+        format: "cjs",
+        generatedCode: {
+            constBindings: true,
+            objectShorthand: true,
+        },
+    },
+};
