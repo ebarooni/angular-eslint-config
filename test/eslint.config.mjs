6b80638a2994e92e5dbcc01a533c3f6b088e69db
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-12-30 21:14:22 +0100

    feat: add eslint config to test project

---
+++ test/eslint.config.mjs
@@ -0,0 +1,10 @@
+import defineEslintConfig from "@ebarooni/angular-eslint-config";
+
+export default defineEslintConfig([{
+  languageOptions: {
+    parserOptions: {
+      project: ["tsconfig.eslint.json"],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+}]);
